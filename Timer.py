f4a5865f5ecaac086629600a5e350570320cb8f6
nip403 <41174986+nip403@users.noreply.github.com>
2018-08-13 03:42:09 +0800

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,14 +1,18 @@
 import time
 
 class Timer:
-    def __init__(self,ability,cooldown):
+    def __init__(self,ability=0,cooldown=0):
+
+        # Ability and cooldown time durations
         self.ability = ability
         self.cooldown = cooldown
 
+        # Period alternates between "waiting", "ability" and "cooldown"
         self.period = "waiting"
         self.t0 = time.time()
         self.time = self.t0
 
+    # Timer handler
     def update(self):
         if not self.period == "waiting":
             self.time = int(time.time()-self.t0)
@@ -19,11 +23,12 @@ class Timer:
             if self.time >= self.cooldown + self.ability and self.period == "cooldown":
                 self.period = "waiting"
 
+    # Begin ability-cooldown cycle
     def start(self):
         if self.period == "waiting":
             self.period = "ability"
             self.t0 = time.time()
-
+            
     @property
     def status(self):
         return self.period
